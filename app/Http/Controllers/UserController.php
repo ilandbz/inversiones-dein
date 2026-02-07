223bdc4255a53a9d42286042716784b6b34e2ac2
Cristian Figueroa Ferrer <ilandbz@gmail.com>
2026-02-07 13:36:04 -0500

    asd

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,14 +20,16 @@ use Illuminate\Support\Facades\Hash;
 use Intervention\Image\ImageManager;
 use Illuminate\Support\Facades\File;
 use Intervention\Image\Drivers\Gd\Driver;
+
 class UserController extends Controller
 {
     use UserFilters;
-    public function store(StoreUserRequest $request){
+    public function store(StoreUserRequest $request)
+    {
         $file = $request->file('foto');
         Persona::firstOrcreate([
             'dni'           => $request->dni,
-            ],[
+        ], [
             'ape_pat'       => $request->apepat,
             'ape_mat'       => $request->apemat,
             'primernombre'  => $request->primernombre,
@@ -63,57 +65,60 @@ class UserController extends Controller
         return response()->json([
             'ok' => 1,
             'mensaje' => 'Usuario Registrado satisfactoriamente'
-        ],200);
+        ], 200);
     }
-    public function cambiarclaveperfil(UpdatePasswordRequest $request){
+    public function cambiarclaveperfil(UpdatePasswordRequest $request)
+    {
         $filters = $this->getUserFilters();
         $user_id = $filters['user_id'];
         $user = Auth::user();
-        if(Hash::check($request->current_password,$user->password)){
+        if (Hash::check($request->current_password, $user->password)) {
             $user = User::find($user_id);
             $user->password = Hash::make($request->password);
             $user->save();
             return response()->json([
                 'ok' => 1,
                 'mensaje' => 'Clave Cambiado con Exito'
-            ],200);
-        }
-        else {
+            ], 200);
+        } else {
             return response()->json([
                 'errors' => [
                     'current_password' => [
                         'Contraseña Incorrecta'
                     ]
                 ]
-            ],422);
+            ], 422);
         }
     }
-    public function cambiarEstado(Request $request){
+    public function cambiarEstado(Request $request)
+    {
         $user = User::find($request->id);
-        if($user->es_activo==0){
-            $user->es_activo=1;
-        }else{
-            $user->es_activo=0;
+        if ($user->es_activo == 0) {
+            $user->es_activo = 1;
+        } else {
+            $user->es_activo = 0;
         }
         $user->save();
         return response()->json([
             'ok' => 1,
             'mensaje' => 'Estado Cambiado'
-        ],200);
+        ], 200);
     }
-    public function resetclave(Request $request){
+    public function resetclave(Request $request)
+    {
         $user = User::where('id', $request->id)->first();
         $user->password = Hash::make($user->name);
         $user->save();
         return response()->json([
             'ok' => 1,
             'mensaje' => 'Clave Reseteado con Exito'
-        ],200);
+        ], 200);
     }
-    public function show(Request $request){
+    public function show(Request $request)
+    {
         $user = User::with(
             'persona:dni,ape_pat,ape_mat,primernombre,otrosnombres,celular'
-            )->where('id', $request->id)->first();
+        )->where('id', $request->id)->first();
         return $user;
     }
     public function cambiarImagen(Request $request)
@@ -144,9 +149,10 @@ class UserController extends Controller
         return response()->json([
             'ok' => 1,
             'mensaje' => 'Imagen Cambiado con Exito'
-        ],200);
+        ], 200);
     }
-    public function update(UpdateUserRequest $request){
+    public function update(UpdateUserRequest $request)
+    {
         $file = $request->file('foto');
         $user = User::findOrFail($request->id);
         $user->update([
@@ -156,7 +162,7 @@ class UserController extends Controller
         $user->save();
         Persona::updateOrCreate([
             'dni'           => $request->dni,
-        ],[
+        ], [
             'ape_pat'       => $request->apepat,
             'ape_mat'       => $request->apemat,
             'primernombre'  => $request->primernombre,
@@ -188,7 +194,7 @@ class UserController extends Controller
         return response()->json([
             'ok' => 1,
             'mensaje' => 'Se guardo Exito'
-        ],200);
+        ], 200);
     }
     public function habilitados(Request $request)
     {
@@ -196,12 +202,12 @@ class UserController extends Controller
         $buscar = mb_strtoupper($request->buscar);
         $paginacion = $request->paginacion;
         $usuarios = User::query()
-        ->whereRaw("upper(name) like ?", ['%' . $buscar . '%'])
-        ->when(is_numeric($buscar), function ($query) use ($buscar) {
-            return $query->orWhere('dni', $buscar);
-        })
-        ->with(['roles', 'agencias'])
-        ->where('es_activo', 1);
+            ->whereRaw("upper(name) like ?", ['%' . $buscar . '%'])
+            ->when(is_numeric($buscar), function ($query) use ($buscar) {
+                return $query->orWhere('dni', $buscar);
+            })
+            ->with(['roles', 'agencias'])
+            ->where('es_activo', 1);
         if ($filters['role'] === 'GERENTE AGENCIA') {
             $usuarios->whereHas('agencias', function ($query) use ($filters) {
                 $query->where('agencia_id', $filters['agencia_id']);
@@ -215,12 +221,12 @@ class UserController extends Controller
         $buscar = mb_strtoupper($request->buscar);
         $paginacion = $request->paginacion;
         $usuarios = User::query()
-        ->whereRaw("upper(name) like ?", ['%' . $buscar . '%'])
-        ->when(is_numeric($buscar), function ($query) use ($buscar) {
-            return $query->orWhere('dni', $buscar);
-        })
-        ->with(['roles', 'agencias'])
-        ->where('es_activo', 0);
+            ->whereRaw("upper(name) like ?", ['%' . $buscar . '%'])
+            ->when(is_numeric($buscar), function ($query) use ($buscar) {
+                return $query->orWhere('dni', $buscar);
+            })
+            ->with(['roles', 'agencias'])
+            ->where('es_activo', 0);
         if ($filters['role'] === 'GERENTE AGENCIA') {
             $usuarios->whereHas('agencias', function ($query) use ($filters) {
                 $query->where('agencia_id', $filters['agencia_id']);
@@ -234,11 +240,11 @@ class UserController extends Controller
         $buscar = mb_strtoupper($request->buscar);
         $paginacion = $request->paginacion;
         $usuarios = User::query()
-        ->whereRaw("upper(name) like ?", ['%' . $buscar . '%'])
-        ->when(is_numeric($buscar), function ($query) use ($buscar) {
-            return $query->orWhere('dni', $buscar);
-        })
-        ->with(['roles', 'agencias']);
+            ->whereRaw("upper(name) like ?", ['%' . $buscar . '%'])
+            ->when(is_numeric($buscar), function ($query) use ($buscar) {
+                return $query->orWhere('dni', $buscar);
+            })
+            ->with(['roles', 'agencias']);
         if ($filters['role'] === 'GERENTE AGENCIA') {
             $usuarios->whereHas('agencias', function ($query) use ($filters) {
                 $query->where('agencia_id', $filters['agencia_id']);
@@ -247,21 +253,21 @@ class UserController extends Controller
         return $usuarios->paginate($paginacion);
     }
     public function todosAsesores()
-    {  
+    {
         $filters = $this->getUserFilters();
         $usuarios = User::query()
-        ->with(['roles', 'agencias', 'persona', 'agencia'])
-        ->where('es_activo', 1)
-        ->whereHas('roles', function ($query) {
-            $query->where('roles.id', 5);
-        });
+            ->with(['roles', 'agencias', 'persona', 'agencia'])
+            ->where('es_activo', 1)
+            ->whereHas('roles', function ($query) {
+                $query->where('roles.id', 5);
+            });
         if ($filters['role'] === 'GERENTE AGENCIA' || $filters['role'] === 'COBRANZA') {
             $usuarios->whereHas('agencias', function ($query) use ($filters) {
                 $query->where('agencia_id', $filters['agencia_id']);
             });
-        }else if($filters['role'] === 'COBRANZA HUANUCO'){
+        } else if ($filters['role'] === 'COBRANZA HUANUCO') {
             $usuarios->whereHas('agencias', function ($query) use ($filters) {
-                $query->whereIn('agencia_id', [1,3]);
+                $query->whereIn('agencia_id', [1, 3]);
             });
         }
         return $usuarios->get();
@@ -270,19 +276,20 @@ class UserController extends Controller
     {
         return User::with('persona')->where('id', $request->id)->first();
     }
-    public function destroy(Request $request){
+    public function destroy(Request $request)
+    {
         $user = User::where('id', $request->id)->first();
-    
-        if ($user && Storage::disk('fotos')->exists('usuarios/'.$user->dni.'.webp')) {
-            Storage::disk('fotos')->delete('usuarios/'.$user->name.'.webp');
+
+        if ($user && Storage::disk('fotos')->exists('usuarios/' . $user->dni . '.webp')) {
+            Storage::disk('fotos')->delete('usuarios/' . $user->name . '.webp');
         }
         $user->delete();
         return response()->json([
             'ok' => 1,
             'mensaje' => 'Usuario eliminado satisfactoriamente'
-        ],200);
+        ], 200);
     }
-    public function obtenerUsuariosOperadores(Request $request)//incluye cobranza y operaciones
+    public function obtenerUsuariosOperadores(Request $request) //incluye cobranza y operaciones
     {
         $filters = $this->getUserFilters();
         $usuarios = User::query()
@@ -318,40 +325,41 @@ class UserController extends Controller
 
         return response()->json($asesores->get());
     }
-    public function mostrarDatoUsuario(Request $request) {
+    public function mostrarDatoUsuario(Request $request)
+    {
         $usuario = User::with([
             'roles',
             'persona',
         ])->where('id', $request->id)->first();
-    
+
         if (!$usuario) {
             return response()->json(['error' => 'Usuario no encontrado'], 404);
         }
-    
+
         $menu = [];
         $roles = $usuario->roles->makeHidden('pivot'); // Oculta pivot de roles
-    
+
         // Ocultar created_at, updated_at y pivot del usuario
         $usuario->makeHidden(['created_at', 'updated_at', 'pivot']);
-    
+
         if ($roles->count() == 1) {
             $role = $roles->first();
             $usuario->setAttribute('role', $role);
             $role->makeHidden(['created_at', 'updated_at', 'pivot']);
-    
+
             $menusPorRol = GrupoMenu::with(['menus' => function ($query) use ($role) {
-                $query->select('id','nombre','slug','icono','grupo_menu_id','padre_menu_id','orden','url')
+                $query->select('id', 'nombre', 'slug', 'icono', 'grupo_menu_id', 'padre_menu_id', 'orden', 'url')
                     ->whereHas('roles', function ($roleQuery) use ($role) {
                         $roleQuery->where('roles.id', $role->id);
                     })->orderBy('orden');
             }])
-            ->whereHas('menus.roles', function ($query) use ($role) {
-                $query->where('roles.id', $role->id);
-            })
-            ->get();
-    
+                ->whereHas('menus.roles', function ($query) use ($role) {
+                    $query->where('roles.id', $role->id);
+                })
+                ->get();
+
             $menu = array_merge($menu, $menusPorRol->toArray());
-    
+
             return response()->json([
                 'usuario' => $usuario,
                 'menus' => $menu,
@@ -362,58 +370,62 @@ class UserController extends Controller
             ], 200);
         }
     }
-    
-    public function obtenerMenusPorRole(Request $request){
+
+    public function obtenerMenusPorRole(Request $request)
+    {
         $menu = [];
-        $role = Role::where('id',$request->role_id)->first();
+        $role = Role::where('id', $request->role_id)->first();
         $menusPorRol = GrupoMenu::with(['menus' => function ($query) use ($role) {
-            $query->select('id','nombre','slug','icono','grupo_menu_id','padre_menu_id','orden')
+            $query->select('id', 'nombre', 'slug', 'icono', 'grupo_menu_id', 'padre_menu_id', 'orden', 'url')
                 ->whereHas('roles', function ($roleQuery) use ($role) {
                     $roleQuery->where('roles.id', $role->id);
                 })
                 ->orderBy('orden');
         }])
-        ->whereHas('menus.roles', function ($query) use ($role) {
-            $query->where('roles.id', $role->id);
-        })
-        ->orderBy('id')
-        ->get();
+            ->whereHas('menus.roles', function ($query) use ($role) {
+                $query->where('roles.id', $role->id);
+            })
+            ->orderBy('id')
+            ->get();
         $menu = array_merge($menu, $menusPorRol->toArray());
         return response()->json([
             'menus' => $menu
-        ],200); 
+        ], 200);
     }
-    public function eliminarRole(Request $request){
+    public function eliminarRole(Request $request)
+    {
         $user = User::find($request->user_id);
         $user->roles()->detach($request->role_id);
         return response()->json([
             'ok' => 1,
             'mensaje' => 'Rol eliminado satisfactoriamente'
-        ],200);
+        ], 200);
     }
-    public function agregarRole(StoreRoleUserRequest $request){
+    public function agregarRole(StoreRoleUserRequest $request)
+    {
         $user = User::find($request->user_id);
         $user->roles()->attach($request->role_id);
         return response()->json([
             'ok' => 1,
             'mensaje' => 'Rol agregado satisfactoriamente'
-        ],200);
+        ], 200);
     }
 
-    public function obtenerPorTipo(Request $request){
+    public function obtenerPorTipo(Request $request)
+    {
         $filters = $this->getUserFilters();
-        $role_id=$request->role_id;
-        $agencia_id=$request->agencia_id;
+        $role_id = $request->role_id;
+        $agencia_id = $request->agencia_id;
 
         $users = User::where('es_activo', 1)
-        ->whereHas('roles', function ($query) use ($role_id) {
-            $query->where('roles.id', $role_id);
-        })
-        ->when($agencia_id != 0, function ($query) use ($agencia_id) {
-            $query->whereHas('agencias', function ($query) use ($agencia_id) {
-                $query->where('agencias.id', $agencia_id);
+            ->whereHas('roles', function ($query) use ($role_id) {
+                $query->where('roles.id', $role_id);
+            })
+            ->when($agencia_id != 0, function ($query) use ($agencia_id) {
+                $query->whereHas('agencias', function ($query) use ($agencia_id) {
+                    $query->where('agencias.id', $agencia_id);
+                });
             });
-        });
         if ($filters['role'] === 'ASESOR') {
             $users->where('id', $filters['user_id']);
         }
@@ -428,5 +440,4 @@ class UserController extends Controller
         })->get();
         return response()->json($roles);
     }
-  
 }
